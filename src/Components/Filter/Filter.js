af5af8795c4d069e23178a616ad938d8a19174ec
Rennan Melo <rennan.melo11@gmail.com>
2021-08-07 06:11:50 -0300

    Refactored Filter component building process

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,55 +1,103 @@
 import "./Filter.css";
 
 const Filter = () => {
+  const selectItems = [
+    {
+      id: "sun",
+      label: {
+        text: "1. Set the amount of sunlight your plant will get.",
+        modifier: "",
+      },
+      options: [
+        {
+          value: "no",
+          text: "No sunlight",
+        },
+        {
+          value: "low",
+          text: "Low sunlight",
+        },
+        {
+          value: "high",
+          text: "High sunlight",
+        },
+      ],
+    },
+    {
+      id: "water",
+      label: {
+        text: "2. How often do you want to water your plant?",
+        modifier: "filter__label--center",
+      },
+      options: [
+        {
+          value: "rarely",
+          text: "Rarely",
+        },
+        {
+          value: "regularly",
+          text: "Regularly",
+        },
+        {
+          value: "daily",
+          text: "Daily",
+        },
+      ],
+    },
+    {
+      id: "pets",
+      label: {
+        text: "3. Do you have pets? Do they chew plants?",
+        modifier: "filter__label--last",
+      },
+      options: [
+        {
+          value: "false",
+          text: "No/They don't care",
+        },
+        {
+          value: "true",
+          text: "Yes",
+        },
+      ],
+    },
+  ];
+
+  const buildOptions = (select) => {
+    return select.options.map((option) => (
+      <option key={option.value} value={option.value}>
+        {option.text}
+      </option>
+    ));
+  };
+
+  const buildLabel = (select) => (
+    <label
+      htmlFor={select.id}
+      className={`filter__label ${select.label.modifier}`}
+    >
+      {select.label.text}
+    </label>
+  );
+
+  const buildSelects = (selectItems) => {
+    return selectItems.map((select) => (
+      <fieldset className="filter__item" key={select.id}>
+        {buildLabel(select)}
+        <select id={select.id} className="filter__select" defaultValue="null">
+          <option disabled value="null">
+            Select...
+          </option>
+          {buildOptions(select)}
+        </select>
+      </fieldset>
+    ));
+  };
+
   return (
     <section id="filter" className="filter">
       <div className="container">
-        <form className="filter__form">
-          <fieldset className="filter__item">
-            <label htmlFor="sun" className="filter__label">
-              <b>1.</b> Set the amount of <b>sunlight</b> your plant will get.
-            </label>
-            <select id="sun" className="filter__select" defaultValue="null">
-              <option disabled value="null">
-                Select...
-              </option>
-              <option value="no">No sunlight</option>
-              <option value="low">Low sunlight</option>
-              <option value="high">High sunlight</option>
-            </select>
-          </fieldset>
-          <fieldset className="filter__item">
-            <label
-              htmlFor="water"
-              className="filter__label filter__label--center"
-            >
-              <b>2.</b> How often do you want to
-              <b>water</b> your plant?
-            </label>
-            <select id="water" className="filter__select" defaultValue="null">
-              <option disabled value="null">
-                Select...
-              </option>
-              <option value="rarely">Rarely</option>
-              <option value="regularly">Regularly</option>
-              <option value="daily">Daily</option>
-            </select>
-          </fieldset>
-          <fieldset className="filter__item">
-            <label htmlFor="pets" className="filter__label filter__label--last">
-              <b>3.</b> Do you have pets? Do they
-              <b>chew</b>
-              plants?
-            </label>
-            <select id="pets" className="filter__select" defaultValue="null">
-              <option disabled value="null">
-                Select...
-              </option>
-              <option value="false">No/They don't care</option>
-              <option value="true">Yes</option>
-            </select>
-          </fieldset>
-        </form>
+        <form className="filter__form">{buildSelects(selectItems)}</form>
       </div>
     </section>
   );
